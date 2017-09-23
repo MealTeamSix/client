6c5e4782e7e6319e34d4a58249ed11a10105ddcc
Cody Engel <cengel815@gmail.com>
2017-09-23 14:03:03 -0500

    Added firebase

--- app/src/main/java/org/chicagosfoodbank/client/LoginActivity.kt
+++ app/src/main/java/org/chicagosfoodbank/client/LoginActivity.kt
@@ -2,10 +2,16 @@ package org.chicagosfoodbank.client
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.View
 import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_login.*
 
+
+
 class LoginActivity : AppCompatActivity() {
+
+    val firebaseAuth = FirebaseAuth.getInstance()
 
     val basicLogin = BasicLogin()
 
@@ -13,12 +19,33 @@ class LoginActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
-        buttonLogin.setOnClickListener {
-            basicLogin.setCredentials(editTextUsername.text.toString(), editTextPassword.text.toString())
-            if (basicLogin.login()) {
-                goToSurvey()
+        if (firebaseAuth.currentUser != null) {
+            buttonLogin.visibility = View.GONE
+
+            println("Current User: ${firebaseAuth.currentUser.toString()}")
+        } else {
+            buttonLogin.visibility = View.VISIBLE
+
+            buttonLogin.setOnClickListener {
+                firebaseAuth.signInAnonymously().addOnCompleteListener {
+                    val task = it
+                    if (task.isSuccessful()) {
+                        // Sign in success, update UI with the signed-in user's information
+                        println("Sign in success!")
+                        val user = firebaseAuth.getCurrentUser()
+                        goToSurvey()
+                    } else {
+                        // If sign in fails, display a message to the user.
+                        println("Sign in broke!")
+                    }
+                }
+//                basicLogin.setCredentials(editTextUsername.text.toString(), editTextPassword.text.toString())
+//                if (basicLogin.login()) {
+//                    goToSurvey()
+//                }
             }
         }
+
     }
 
     fun goToSurvey() {
